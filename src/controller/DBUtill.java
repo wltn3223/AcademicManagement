fa522bdb7d560a8a7e71d47154f8ad6edfc9570c
Mun Jisu <answltn8@gmail.com>
2023-10-19 16:45:37 +0900

    java db 연동(javauser)

--- src/controller/DBUtill.java
+++ src/controller/DBUtill.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class DBUtill {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBUtill {
+	static final String driver = "oracle.jdbc.driver.OracleDriver";
+	static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	
+	public static Connection getConnection() throws Exception {
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, "javauser", "java1234");
+//		 System.out.println("DB 연결 성공");
+		return con;
+		}
+	
 }
